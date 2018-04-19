1aa93512f52f32f7c4ae99385dd1cdaa8b29501c
Elena Lepilkina <elepilk@synopsys.com>
2018-04-19 14:56:16 +0300

    Fix compilers. 2

--- .travis/run.sh
+++ .travis/run.sh
@@ -11,13 +11,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
-    CC=$C_COMPILER
-    CXX=$CXX_COMPILER
+    CC=$COMPILER
+    CXX=$COMPILER
 fi
 
 mkdir build && cd build
 conan remote add conan.io https://server.conan.io False
-conan install .. -s build_type=$BUILD_TYPE --build=missing
+conan install .. -s build_type=$BUILD_TYPE --build=missing 
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$COMPILER -DCMAKE_CXX_COMPILER=$COMPILER
 cmake --build . -- VERBOSE=1
 ctest -V
