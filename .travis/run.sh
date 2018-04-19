8bac164309fd2405d2c2891b8af2cbc9af4d66df
Elena Lepilkina <elepilk@synopsys.com>
2018-04-19 14:47:54 +0300

    Fix compilers

--- .travis/run.sh
+++ .travis/run.sh
@@ -18,6 +18,6 @@ fi
 mkdir build && cd build
 conan remote add conan.io https://server.conan.io False
 conan install .. -s build_type=$BUILD_TYPE --build=missing
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$COMPILER -DCMAKE_CXX_COMPILER=$COMPILER
 cmake --build . -- VERBOSE=1
 ctest -V
